3981cd1656176dd52d48dc756486f80e4f714752
aliroohan <aliroohan321@gmail.com>
2024-03-01 23:07:56 +0500

    Created objects in maon function

--- Task1/StudentTest.java
+++ Task1/StudentTest.java
@@ -1,4 +1,185 @@
 package Task1;
+
+class Address {
+    private String street;
+    private String town;
+    private String city;
+    private String country;
+    private PhoneNumber phoneNumber;
+
+    public Address(String street, String town, String city, String country, PhoneNumber phoneNumber) {
+        setStreet(street);
+        setTown(town);
+        setCity(city);
+        setCountry(country);
+        setPhoneNumber(phoneNumber);
+    }
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
+
+
+class Course {
+    private String courseCode;
+    private String courseTitle;
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+    
+    public String getCourseCode() {
+        return courseCode;
+    }
+    
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+    
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+}
+
+
+class PhoneNumber {
+    private String countryCode;
+    private String cityCode;
+    private String lineNumber;
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(String lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+    public PhoneNumber(String countryCode, String cityCode, String lineNumber) {
+        setCountryCode(countryCode);
+        setCityCode(cityCode);
+        setLineNumber(lineNumber);
+        
+    }
+}
+
+
+class Student {
+    private String name;
+    private String email;
+    private String CNIC;
+    private Course course1;
+    private Course course2;
+    private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCNIC() {
+        return CNIC;
+    }
+
+    public void setCNIC(String CNIC) {
+        this.CNIC = CNIC;
+    }
+
+    public Course getCourse1() {
+        return course1;
+    }
+
+    public void setCourse1(Course course1) {
+        this.course1 = course1;
+    }
+
+    public Course getCourse2() {
+        return course2;
+    }
+
+    public void setCourse2(Course course2) {
+        this.course2 = course2;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Student(String name,String CNIC, Address address) {
+        setName(name);
+        setCNIC(CNIC);
+        setAddress(address);
+    }
+}
+
+
 public class StudentTest {
     public static void main(String[] args) {
     Student student1 = new Student("Ali", "12345-1234567-1", new Address("Street 1", "Town 1", "City 1", "Country 1", new PhoneNumber("92", "51", "1234567")));
